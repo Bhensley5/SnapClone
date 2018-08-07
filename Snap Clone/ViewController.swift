219cd25a935cf9d99cf1fd37ade21f7a772f13ec
Brandon Hensley <bhensley5@gmail.com>
2018-08-07 15:18:25 -0400

    Initial Commit

---
+++ Snap Clone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snap Clone
+//
+//  Created by Brandon Hensley on 8/7/18.
+//  Copyright © 2018 Brandon Hensley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
